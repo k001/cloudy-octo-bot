ec14d99062bd2be3d70f6c294065a300f8a184c1
Ivan Zenteno <k001@users.noreply.github.com>
2016-03-09 14:26:02 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-/*
-Copyright (C) 2016 Electronic Arts Inc.  All rights reserved.
-  
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions
-are met:
-  
-1.  Redistributions of source code must retain the above copyright
-    notice, this list of conditions and the following disclaimer.
-2.  Redistributions in binary form must reproduce the above copyright
-    notice, this list of conditions and the following disclaimer in the
-    documentation and/or other materials provided with the distribution.
-3.  Neither the name of Electronic Arts, Inc. ("EA") nor the names of
-    its contributors may be used to endorse or promote products derived
-    from this software without specific prior written permission.
-  
-THIS SOFTWARE IS PROVIDED BY ELECTRONIC ARTS AND ITS CONTRIBUTORS "AS IS" AND ANY
-EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL ELECTRONIC ARTS OR ITS CONTRIBUTORS BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
 var tokens = {
   wit: {
     witToken: process.env.WIT_TOKEN
@@ -49,11 +22,6 @@ var tokens = {
     username: process.env.ATTSN_USERNAME,
     password: process.env.ATTSN_PASSWORD,
     hostname: process.env.ATTSN_HOST
-  },
-  redis: {
-    hostname: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-    namespace: process.env.REDIS_NAMESPACE
   }
 };
 if (!tokens) {
@@ -64,18 +32,11 @@ if (!tokens) {
 var Botkit = require('botkit');
 var Witbot = require('witbot');
 var os = require('os');
-var winston = require('winston');
 var fs = require('fs');
 
-var redisConfig = {
-  port: tokens.redis.port,
-  host: tokens.redis.hostname,
-  namespace: tokens.redis.namespace
-};
-var redisStorage = require('botkit-storage-redis')(redisConfig);
 var controller = Botkit.slackbot({
-  debug: tokens.slack.debug,
-  logLevel: 'info',
+  debug: (tokens.slack.debug)? 'debug': 'info',,
+  logLevel: 'debug',
   json_file_store:  '/tmp/data'
 });
 
